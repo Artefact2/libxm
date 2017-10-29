d7000267621160b8ba42e055b0a16b6bd3017293
Artefact2 <artefact2@gmail.com>
2017-10-29 16:01:15 +0100

    Procedurally generate waveforms in xmprocdemo

--- examples/xmprocdemo/xmprocdemo.c
+++ examples/xmprocdemo/xmprocdemo.c
@@ -15,6 +15,39 @@ static snd_pcm_t* device;
 static snd_pcm_hw_params_t* params;
 static float buffer[4096];
 
+static void gen_waveforms(void) {
+	size_t i;
+	int8_t* buf;
+	size_t len;
+	uint8_t bits;
+
+	/* Square, large duty, half volume */
+	buf = xm_get_sample_waveform(ctx, 1, 0, &len, &bits);
+	for(i = 0x40; i < len; ++i) buf[i] = 127;
+
+	/* Ramp */
+	buf = xm_get_sample_waveform(ctx, 2, 0, &len, &bits);
+	for(i = 0; i < len; ++i) buf[i] = (256 * i) / len - 128;
+
+	/* Square, small duty */
+	buf = xm_get_sample_waveform(ctx, 4, 0, &len, &bits);
+	for(i = 0; i < 0x80; ++i) buf[i] = -128;
+	for(; i < 0xB0; ++i) buf[i] = 127;
+	for(; i < len; ++i) buf[i] = -128;
+
+	/* XXX: Kick */
+	/* XXX: Pad */
+	/* XXX: Drum */
+
+	/* Noise */
+	buf = xm_get_sample_waveform(ctx, 8, 0, &len, &bits);
+	unsigned int next = 1;
+	for(i = 0; i < len; ++i) {
+		next = next * 8127 + 1; /* A very simple linear congruence generator, see rand(3) */
+		buf[i] = next >> 16 & 0xFF;
+	}
+}
+
 int main(void) {
 	unsigned int rate = 48000;
 
@@ -28,6 +61,8 @@ int main(void) {
 	snd_pcm_hw_params(device, params);
 
 	xm_create_context_from_libxmize(&ctx, mus, rate);
+	gen_waveforms();
+
 	snd_pcm_prepare(device);
 
 	while(1) {
