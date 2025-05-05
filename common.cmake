7384e0293d1514826a1aa664b014d21e5273840a
Romain “Artefact2” Dal Maso <romain.dalmaso@artefact2.com>
2025-05-05 17:54:49 +0200

    Check for gcc/clang before using fancy compiler flags

--- common.cmake
+++ common.cmake
@@ -8,14 +8,15 @@ set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
 # Link statically by default
 option(BUILD_SHARED_LIBS "Build using shared libraries" OFF)
 
-# XXX: not portable
-add_compile_options(
-	"$<$<CONFIG:MinSizeRel>:-Oz>"
-	"$<$<CONFIG:MinSizeRel>:-ffast-math>"
-	-Wall -Wextra -Wpadded -Wdouble-promotion -Wpedantic
-)
-add_link_options(
-	"$<$<CONFIG:MinSizeRel>:-z>"
-	"$<$<CONFIG:MinSizeRel>:norelro>"
-	"$<$<CONFIG:MinSizeRel>:-Wl,--build-id=none>"
-)
+if(CMAKE_C_COMPILER_ID MATCHES "^(GNU|Clang)$")
+	add_compile_options(
+		"$<$<CONFIG:MinSizeRel>:-Oz>"
+		"$<$<CONFIG:MinSizeRel>:-ffast-math>"
+		-Wall -Wextra -Wpadded -Wdouble-promotion -Wpedantic
+	)
+	add_link_options(
+		"$<$<CONFIG:MinSizeRel>:-z>"
+		"$<$<CONFIG:MinSizeRel>:norelro>"
+		"$<$<CONFIG:MinSizeRel>:-Wl,--build-id=none>"
+	)
+endif()
